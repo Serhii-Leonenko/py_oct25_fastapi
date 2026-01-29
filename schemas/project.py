eaf873886cabe508ad54dd07baacff5a04893295
Serhii-Leonenko <leonenko3149@gmail.com>
2026-01-29 11:44:14 +0200

    added schemas

---
+++ schemas/project.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, Field, ConfigDict
+
+from schemas.task import TaskReadSchema
+from schemas.user import UserReadSchema
+
+
+class ProjectCreateSchema(BaseModel):
+    name: str = Field(min_length=1, max_length=100)
+    description: str | None = None
+    owner: int
+
+
+class ProjectReadSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    name: str
+    description: str | None = None
+    owner: UserReadSchema
+    tasks: list[TaskReadSchema]
---
+++ schemas/task.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, Field, ConfigDict
+
+from models.enums import TaskStatusEnum
+from schemas.user import UserReadSchema
+
+
+class TaskBaseSchema(BaseModel):
+    title: str = Field(min_length=1, max_length=200)
+    description: str | None = None
+
+
+class TaskCreateSchema(TaskBaseSchema):
+    assignees: list[int] = Field(default_factory=list)
+    project: int
+
+
+class TaskUpdateSchema(BaseModel):
+    status: TaskStatusEnum
+    assignees: list[int] = Field(default_factory=list)
+
+
+class TaskReadSchema(TaskBaseSchema):
+    model_config = ConfigDict(
+        from_attributes=True
+    )
+
+    id: int
+    status: TaskStatusEnum
+    assignees: list[UserReadSchema]
